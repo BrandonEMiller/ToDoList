15dbbd68873e9ebe647490662a9b124919769e61
Brandon Miller <brandonmiller@unknown84383564ec6c.gateway.2wire.net>
2013-09-13 12:58:11 -0400

    With deployable version

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,40 @@
 /*global describe, it */
 'use strict';
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-
-            });
+  describe('The To Do List', function(){
+    this.timeout(15000);
+ 
+    it('should save a new task and that task should be returned from Parse', function(done){
+      var result;
+ 
+      // fill out the form
+      var form = $('.form')
+      // make a random title with which we can query
+      var randomTask = 'A Test Task #'+ Math.floor(Math.random()*10000000)
+      form.find('#task').val(randomTask)
+      form.find('#content').val('This is a really great post! I loved writing it!')
+ 
+      // submit it
+      $('.save').click()
+ 
+      setTimeout((function(){
+ 
+        var query = new Parse.Query(TaskClass);
+        query.equalTo("task", randomTask);
+        query.find({
+          success: function(results) {
+            result = results[0]
+            console.log(results)
+            expect(result.get('task')).to.equal(randomTask)
+            done()
+          },
+          error: function(error) {
+            done(error.description)
+          }
         });
-    });
+ 
+      }), 2000)
+    }); // end it()
+ 
+  })
 })();
