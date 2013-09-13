371d7b9524f804402f71f44427dbbe20f9e3f6a5
Brandon Miller <brandonmiller@unknown84383564ec6c.gateway.2wire.net>
2013-09-13 11:58:20 -0400

    First Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,79 @@
+Parse.initialize("T9FwiJMfIbPmrG65XR5x9TCDZPhFo59WvCx4RvBR", "FHRUtYc5Cc02z6oiHbejQPM7G2jvXavLjnX23Eel");
+
+var TaskClass = Parse.Object.extend("Task");
+ 
+var TaskCollectionClass = Parse.Collection.extend({
+  model: TaskClass
+});
+ 
+// create an instance of the collection
+var task = new TaskCollectionClass()
+ 
+task.fetch({
+  success: function(collection) {
+    collection.each(function(task){
+      addToSideBar(task)
+    })
+  }
+})
+
+$('.save').click(function(){
+	console.log("saving")
+  var task = new TaskClass();
+  task.set('task', $('#task').val());
+  task.set('content', $('#content').val());
+ 
+  task.save(null, {
+    success: function(result){
+      addToSideBar(result)
+    },
+    error: function(result, error){
+      alert("Failure, "+ error.description)
+    }
+  })
+})
+
+
+
+ 
+// functions 
+
+
+ 
+function addToSideBar(task) {
+  var li = $('<li>'+task.get('task')+'</li>')
+  $('.taskList').append(li)
+  li.click(function(){
+    renderTask(task)
+  })
+}
+ 
+function renderTask(task) {
+  $('#task').val( task.get('task') )
+  $('#content').val( task.get('content') )
+  $('.delete').click(function(){
+  		task.destroy({
+  			success: function(task){
+  				 
+  			},
+  			error: function(task, error){
+
+  			}
+  		})
+
+	})
+  $('.update').click(function(){
+  	taskTitle = $('#task').val()
+  	taskContent = $('#content').val()
+
+  	task.save(null, {
+  		success: function(task) {
+    
+   			task.set("task", taskTitle);
+    		task.set("content", taskContent);
+    		task.save();
+    		
+  		}
+	});
+  })
+}
